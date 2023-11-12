1e0c2689d3627d2d93839e514aed5d8272645b6d
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-11-12 11:02:14 +1100

    Main loop function created.

---
+++ hangPy.py
@@ -0,0 +1,10 @@
+# hangPy.py is a Python implementation of the classic game Hangman.
+# created by: Andrew Fong on 12/11/2023
+
+
+def main():
+    pass  # Placeholder for the main game loop
+
+
+if __name__ == "__main__":
+    main()
