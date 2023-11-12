b8be4afbc998d9ab5b166673cd4ccf764911714f
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-11-12 11:06:15 +1100

    Added words to list, implemented get_random_word

--- hangPy.py
+++ hangPy.py
@@ -1,18 +1,36 @@
+from random import choice
+
 # hangPy.py is a Python implementation of the classic game Hangman.
 # created by: Andrew Fong on 12/11/2023
 
 
 def main():
-    word_list = []  # List to store the words
+    word_list = [
+        "apple",
+        "banana",
+        "cherry",
+        "orange",
+        "grape",
+        "kiwi",
+        "lemon",
+        "melon",
+        "peach",
+        "pear",
+    ]  # List to store the words
     guessed_letters = set()  # Set to store guessed letters
+    secret_word = get_random_word(word_list)
 
 
-def get_random_word():
-    pass  # Placeholder for word selection function
+def get_random_word(word_list):
+    return choice(word_list)
 
 
 def get_player_guess():
     pass  # Placeholder for player's guess input function
+
+
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == "__main__":
